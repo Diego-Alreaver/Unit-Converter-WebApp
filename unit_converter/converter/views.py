685400df9370c9f9c5652a78a0a3c53edcf80bf7
Diego-Alreaver <diego8558@hotmail.com>
2024-09-28 11:44:02 -0400

    añadido conversor de peso

--- unit_converter/converter/views.py
+++ unit_converter/converter/views.py
@@ -2,7 +2,15 @@
 from django.shortcuts import render
 
 # Diccionario con las conversiones a metros
-CONVERSION_FACTORS = {
+CONVERSION_FACTORS_LENGTH = {
+    'meter': 1,
+    'kilometer': 1000,
+}
+CONVERSION_FACTORS_WEIGHT = {
+    'gram': 1,
+    'kilogram': 1000,
+}
+CONVERSION_FACTORS_TEMPERATURE = {
     'meter': 1,
     'kilometer': 1000,
 }
@@ -17,10 +25,10 @@ def length_conversion(request):
         to_unit = request.POST.get('to_unit')
 
         # Convertir de la unidad origen a metros
-        value_in_meters = value * CONVERSION_FACTORS[from_unit]
+        value_in_meters = value * CONVERSION_FACTORS_LENGTH[from_unit]
 
         # Convertir de metros a la unidad destino
-        converted_value = value_in_meters / CONVERSION_FACTORS[to_unit]
+        converted_value = value_in_meters / CONVERSION_FACTORS_LENGTH[to_unit]
 
         context = {
             'converted_value': converted_value,
@@ -32,11 +40,24 @@ def length_conversion(request):
     return render(request, 'converter/length.html', context)
 
 def weight_conversion(request):
+    context = {}
     if request.method == 'POST':
-        # Lógica para convertir pesos
-        context = {'converted_value': "Resultado de peso"}
-    else:
-        context = {}
+        value = float(request.POST.get('value'))
+        from_unit = request.POST.get('from_unit')
+        to_unit = request.POST.get('to_unit')
+
+        # Convertir de la unidad origen a gramos
+        value_in_meters = value * CONVERSION_FACTORS_WEIGHT[from_unit]
+
+        # Convertir de gramos a la unidad destino
+        converted_value = value_in_meters / CONVERSION_FACTORS_WEIGHT[to_unit]
+
+        context = {
+            'converted_value': converted_value,
+            'from_unit': from_unit,
+            'to_unit': to_unit,
+            'value': value,
+        }
     return render(request, 'converter/weight.html', context)
 
 def temperature_conversion(request):
